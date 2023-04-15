a4b5b3c16834756d3df364de181dd0a77f2b529c
LeandroYara <l.yara@uniandes.edu.co>
2023-04-15 11:37:50 -0500

    Practica de semana terminada

--- src/create/prefab_creator.py
+++ src/create/prefab_creator.py
@@ -2,7 +2,7 @@ import random
 import pygame
 import esper
 from src.ecs.components.c_enemy_spawner import CEnemySpawner
-
+from src.ecs.components.c_input_command import CInputCommand
 from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
 from src.ecs.components.c_velocity import CVelocity
@@ -32,14 +32,14 @@ def create_enemy_square(world:esper.World, pos:pygame.Vector2, enemy_info:dict):
     velocity = pygame.Vector2(random.choice([-vel_range, vel_range]),
                               random.choice([-vel_range, vel_range]))
     enemy_entity = create_square(world, size, pos, velocity, color)
-    world.add_component(enemy_entity, CTagEnemy)
+    world.add_component(enemy_entity, CTagEnemy())
 
 def create_enemy_spawner(world:esper.World, level_data:dict):
     spawner_entity = world.create_entity()
     world.add_component(spawner_entity,
                         CEnemySpawner(level_data["enemy_spawn_events"]))
     
-def create_player_square(world:esper.World, player_info:dict, player_lvl_info:dict):
+def create_player_square(world:esper.World, player_info:dict, player_lvl_info:dict) -> int:
     size = pygame.Vector2(player_info["size"]["x"],
                           player_info["size"]["y"])
     color = pygame.Color(player_info["color"]["r"],
@@ -49,4 +49,19 @@ def create_player_square(world:esper.World, player_info:dict, player_lvl_info:di
                          player_lvl_info["position"]["y"] - (size.y / 2))
     vel = pygame.Vector2(0,0)
     player_entity = create_square(world, size, pos, vel, color)
-    world.add_component(player_entity, CTagPlayer)
+    world.add_component(player_entity, CTagPlayer())
+    return player_entity
+
+def create_input_player(world:esper.World):
+    input_left = world.create_entity()
+    input_right = world.create_entity()
+    input_up = world.create_entity()
+    input_down = world.create_entity()
+    world.add_component(input_left,
+                        CInputCommand("PLAYER_LEFT", pygame.K_LEFT))
+    world.add_component(input_right,
+                        CInputCommand("PLAYER_RIGHT", pygame.K_RIGHT))
+    world.add_component(input_up,
+                        CInputCommand("PLAYER_UP", pygame.K_UP))
+    world.add_component(input_down,
+                        CInputCommand("PLAYER_DOWN", pygame.K_DOWN))
